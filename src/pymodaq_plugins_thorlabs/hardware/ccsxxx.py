3cb5c7f9442bd4fc36c2f1a9a27d1d3dfe2f1162
nano713 <50872819+nano713@users.noreply.github.com>
2024-10-31 10:19:03 +0900

    Add ccsxxx.py

---
+++ src/pymodaq_plugins_thorlabs/hardware/ccsxxx.py
@@ -0,0 +1,10 @@
+import clr
+import sys
+
+from System import Decimal
+from System import Action
+from System import UInt64
+from System import UInt32
+
+ccs_path = 'C:\\Program Files\\Thorlabs\\Kinesis'
+sys.path.append(ccs_path)
