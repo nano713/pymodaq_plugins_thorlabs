c2f19753aa6938540e9e17d061761a389e68b4c2
Daichi Kozawa <kozawa.daichi@nims.go.jp>
2025-01-10 23:01:18 +0900

    Refactor
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- src/pymodaq_plugins_thorlabs/hardware/elliptec.py
+++ src/pymodaq_plugins_thorlabs/hardware/elliptec.py
@@ -1,6 +1,7 @@
 import sys, os, time, logging
 import clr 
-from System import Decimal
+from System import Decimal, Char
+from System.Collections.Generic import List
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +12,6 @@ clr.AddReference("Thorlabs.Elliptec.ELLO_DLL")
 
 from Thorlabs.Elliptec.ELLO_DLL import ELLDevicePort, ELLDevices, ELLBaseDevice, DeviceID # DK - is this correct? I thought ELLO_DLL class comes from dll.
                                                                                         # AD -> No. These are the classes. ELLO_DLL is the name of the dll not the class
-
 class Elliptec: 
     def __init__(self): 
         self.controller = ELLDevices()
@@ -22,42 +22,50 @@ class Elliptec:
         self.max_add = 'F'
         self.device_address = []
 
-    def pick_device(self, device_address):
-        boolean = self.controller.ReaddressDevice(device_address)
-        if boolean == True: 
-            if device_address not in self.device_address: 
-                self.device_address.append(device_address)
-        else: 
-            logger.error('Failed to pick device')
+    # def pick_device(self, device_address):
+    #     boolean = self.controller.ReaddressDevice(device_address)
+    #     if boolean == True: 
+    #         if device_address not in self.device_address: 
+    #             self.device_address.append(device_address)
+    #     else: 
+    #         logger.error('Failed to pick device')
         
-
-    
     def connect(self, com_port): 
        ELLDevicePort.Connect(com_port) # com_port=COM12        
            
        elliptec = self.controller.ScanAddresses(self.min_add, self.max_add)
+       
        logger.info(f'Connected to Elliptec device at {com_port}')
        
        for device in elliptec: 
            if self.controller.Configure(device): 
                 self.elliptec_list = self.controller.AddressedDevice(device[0])
+                self.device_address.append(self.convert_to_char(self.elliptec_list.Address))
                 self.device_info = self.elliptec_list.DeviceInfo
                 for stri in self.device_info.Description():
                     print(stri)
-                #logger.info(f'Device info: {device_info}')
+
+    def convert_to_char(self, address): 
+        char_list = List[Char]()
+        char_list.Add(Char.Parse(address))
+        return char_list
 
     #ELLBaseDevice or ELLDevices? 
     def move_abs(self, actuator, value): 
-        self.elliptec_list.MoveAbsolute(self.device_address[actuator-1], Decimal(value))
+        boolean = self.elliptec_list.MoveAbsolute(self.device_address[actuator-1], Decimal(value)) # DK - I think the first attribute should be '1'/'2' or 1/2 because this is supposed to be 'address' in the documentation. 
+        return boolean
     
     def move_rel(self, actuator, value): 
-        self.elliptec_list.MoveRelative(self.device_address[actuator-1], Decimal(value))
+        boolean = self.elliptec_list.MoveRelative(self.device_address[actuator-1], Decimal(value))
+        return boolean
     
     def home(self, actuator): 
-        if self.get_device_type == "LinearStage":
-            self.elliptec_list.Home(self.device_address[actuator-1], ELLBaseDevice.DeviceDirection.Linear)
-        elif self.get_device_type == "RotaryStage":
-            self.elliptec_list.Home(self.device_address[actuator-1], ELLBaseDevice.DeviceDirection.Clockwise)
+        if "LinearStage" in self.get_device_type:
+            boolean = self.elliptec_list.Home(self.device_address[actuator-1], ELLBaseDevice.DeviceDirection.Linear)
+            return boolean
+        elif "RotaryStage" in self.get_device_type:
+            boolean = self.elliptec_list.Home(self.device_address[actuator-1], ELLBaseDevice.DeviceDirection.Clockwise)
+            return boolean
         else:
             logger.error(f'Unknown device type: {self.get_device_type}')
 
@@ -67,23 +75,7 @@ class Elliptec:
 
     def get_device_type(self, actuator): 
         """Get the device type of the connected device"""
-        return str(self.device_info.get_DeviceType()) + str(actuator)
+        return str(self.device_info.get_DeviceType()) +  str(actuator)
 
-    def get_units(self): 
+    def get_units(self):  # DK - add address 
         return self.device_info.Units
-            
-    # method to get device type
-    
-    # method to set units
-    
-    
-    """
-    some random idea (No: I hope you do not polish this)
-    device_list = []
-    for i in total_number_of_devices:
-        device_list = ellDevices.AddressedDevice(device[i])
-    
-    device_list[axis_index] # to specify an axis.
-    
-    what do you think?
-    """
