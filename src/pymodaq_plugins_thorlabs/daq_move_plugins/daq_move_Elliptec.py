bbe5b93d2caeb7a07b7a36ba4376c94962a18580
adeshazer123 <adeshazer@wisc.edu>
2025-01-13 20:16:55 -0600

    Rename _controller_units

--- src/pymodaq_plugins_thorlabs/daq_move_plugins/daq_move_Elliptec.py
+++ src/pymodaq_plugins_thorlabs/daq_move_plugins/daq_move_Elliptec.py
@@ -30,10 +30,9 @@ class DAQ_Move_Elliptec(DAQ_Move_base):
          hardware library
 
     """
-    _controller_units = ' '
     is_multiaxes = True
     _axis_names: Union[List[str], Dict[str, int]] = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, '11': 11, '12': 12, '13': 13, '14': 14, '15': 15, '16': 16, '17': 17}
-    _controller_units: Union[str, List[str]] = ''
+    _controller_units: Union[str, List[str]] = ' '
     _epsilon: Union[float, List[float]] = 0.1
 
     params = [ {'title': 'COM port', 'name': 'com_port', 'type': 'str', 'value': 'COM12'},
